7dd10d80549aab04841fdcd192902b1b096944ab
Ryan Jagger <ryanjagger@gmail.com>
2015-12-18 21:47:34 -0600

    Established mysql connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,14 @@
 var mysql = require('mysql');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: ''
+});
 
-
+connection.connect(function(err){
+  if(err){
+    console.error('there was an error connecting ', err.stack);
+  }
+  console.log('connected as id: ', connection.threadId);
+});
