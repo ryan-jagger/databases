e66cedddacec55026af640f711f5cef9c2a487da
Ryan Jagger <ryanjagger@gmail.com>
2015-12-19 15:30:17 -0600

    Schemas updated

--- server/schema.sql
+++ server/schema.sql
@@ -1,47 +1,28 @@
--- DROP Database chat;
+DROP Database chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE users (
-  u_id INT(11) NOT NULL AUTO_INCREMENT,
+  user_id INT(11) NOT NULL AUTO_INCREMENT,
   username varchar(20) NOT NULL,
-  PRIMARY KEY (u_id)
+  PRIMARY KEY (user_id)
 )ENGINE=INNODB;
 
-CREATE TABLE rooms (
-  r_id INT(11) NOT NULL AUTO_INCREMENT,
-  roomname varchar(20) NOT NULL,
-  PRIMARY KEY (r_id)
-)ENGINE=INNODB;
+-- CREATE TABLE rooms (
+--   r_id INT(11) NOT NULL AUTO_INCREMENT,
+--   roomname varchar(20) NOT NULL,
+--   PRIMARY KEY (r_id)
+-- )ENGINE=INNODB;
 
 CREATE TABLE messages (
-  m_id INT(11) NOT NULL AUTO_INCREMENT,
-  text TEXT(255) NOT NULL,
-  created_at DATETIME(6) NOT NULL,
-  roomname INT(11) NOT NULL,
+  message_id INT(11) NOT NULL AUTO_INCREMENT,
+  message_text TEXT(255) NOT NULL,
+  created_at INT(11) NOT NULL,
+  roomname VARCHAR(20),
   username INT(11) NOT NULL,
-  PRIMARY KEY (m_id),
-  INDEX(roomname),
+  PRIMARY KEY (message_id),
   INDEX(username),
-  FOREIGN KEY (roomname) REFERENCES rooms(r_id),
-  FOREIGN KEY (username) REFERENCES users(u_id)
+  -- FOREIGN KEY (roomname) REFERENCES rooms(r_id),
+  FOREIGN KEY (username) REFERENCES users(user_id)
 )ENGINE=INNODB;
-
-"insert into messages(username, roomname, text, created_at) 
-  values(
-    (if not exists( select from users(username) where username='tyler' ) 
-      begin 
-        insert into users(username) values('tyler');
-        select from users(u_id) where username='tyler';
-    else 
-      begin
-        select from users(u_id) where username='tyler';),
-    (if not exists( select from room(r_id) where roomname='someRoom')
-      begin
-        insert into rooms(roomname) values ('someRoom');
-        select from rooms(r_id) where roomname='someRoom';
-      else
-        begin
-          select from rooms(r_id) where roomname='someRoom';),
-    'this is a message', UNIX_TIMESTAMP());"
--- server/schema2.sql
+++ server/schema2.sql
@@ -5,7 +5,14 @@ USE chat2;
 CREATE TABLE messages(
     message_id INT(11) not null auto_increment primary key,
     message_text text(255),
-    username varchar(20),
+    username INT(11) not null,
     roomname varchar(20),
-    created_at int(11)
-);
+    created_at int(11),
+    INDEX(username),
+    FOREIGN KEY (username) REFERENCES users(user_id)
+)ENGINE=INNODB;
+
+CREATE TABLE users(
+   user_id INT(11) not null auto_increment primary key,
+   username VARCHAR(20) UNIQUE
+)ENGINE=INNODB;
