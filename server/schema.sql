e0214c6993a89e3865e36a59a28c21f6975e3f81
Ryan Jagger <ryanjagger@gmail.com>
2015-12-18 21:14:53 -0600

    Finished schema design

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,33 @@
+DROP Database chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  u_id INT(11) NOT NULL AUTO_INCREMENT,
+  username varchar(20) NOT NULL,
+  PRIMARY KEY (u_id)
+)ENGINE=INNODB;
+
+CREATE TABLE rooms (
+  r_id INT(11) NOT NULL AUTO_INCREMENT,
+  roomname varchar(20) NOT NULL,
+  PRIMARY KEY (r_id)
+)ENGINE=INNODB;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+  m_id INT(11) NOT NULL AUTO_INCREMENT,
+  text TEXT(255) NOT NULL,
+  created_at DATETIME(6) NOT NULL,
+  roomname INT(11) NOT NULL,
+  username INT(11) NOT NULL,
+  PRIMARY KEY (m_id),
+  INDEX(roomname),
+  INDEX(username),
+  FOREIGN KEY (roomname) REFERENCES rooms(r_id),
+  FOREIGN KEY (username) REFERENCES users(u_id)
+)ENGINE=INNODB;
 
-/* Create other tables and define schemas for them here! */
+-- ALTER TABLE `messages` ADD
 
-
-
-
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+-- ALTER TABLE `messages` ADD CONSTRAINT `messages_fk1`
